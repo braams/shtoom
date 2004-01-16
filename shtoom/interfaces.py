65718e18ec6f77a927bf9169274b7df115cd0388
anthonybaxter <>
2004-01-16 16:31:48 +0000

    updated

--- shtoom/interfaces.py
+++ shtoom/interfaces.py
@@ -3,67 +3,80 @@
 from twisted.python.components import Interface
 
 
-class IPhoneUIFeedback(Interface):
-    """ User interface glue layer.
-
-        The phone calls back into this to make 'things happen' in the UI
+class UI(Interface):
+    """ This interface describes the user interface calls that can be
+        invoked by the Application. Note that "user interface" does not
+        necessarily mean some sort of graphical interface - for instance
+        a conferencing server's UI would be a programmatic thing.
     """
 
     def debugMessage(self, message):
         """ A debugging message.
-
-            UI hint: some sort of text widget
         """
 
     def statusMessage(self, message):
-        """ A status message.
-
-            UI hint: some sort of text label
+        """ Displays a status message. This will typically replace any
+            previous status message.
         """
 
-    def callConnected(self, call):
-        """ The call 'call' has been connected
+    def callConnected(self, cookie):
+        """ The call identified by 'cookie' has been connected
         """
 
-    def callDisconnected(self, call):
-        """ The call 'call' has been disconnected
+    def callDisconnected(self, cookie, reason):
+        """ The call identified by 'cookie' has been disconnected. 'reason'
+            gives a short text description for the call failure.
         """
 
-    def incomingCall(self, description, call, defresp, defsetup):
+    def incomingCall(self, description, cookie, defresp):
         """ An incoming call (described by text 'description') has arrived.
 
             The UI should call defresp.callback() to accept the call,
             and defresp.errback() to reject it.
 
-            defsetup is a deferred which will be triggered when call setup
-            is complete or failed, as in the response to placeCall()
-
-            XXX should pass reject reasons back.
+            The Application will call 'callConnected()' when the call
+            is setup, or callDisconnected() if the call could not be
+            established.
         """
 
-
-class ISipPhone(Interface):
-    """ SIP glue layer. The UI keeps a reference to a ISipPhone, and
-        invokes it's methods to do stuff.
+class SIP(Interface):
+    """ This describes the interface to the SIP layer that the Application
+        uses. 
     """
 
-    def placeCall(self, url):
-        """ Place a call to 'url'
-
-            Returns callid, a string
+    def placeCall(self, sipURL):
+        """ Place a call to URL 'sipURL'.
         """
 
-    def dropCall(self, callid):
-        """ Drop call identified by 'callid'
+class RTP(Interface):
+    """ This describes the interface to the RTP layer used by the Application
+    """
+
+    def __init__(self, cookie):
+        """ Create an RTP instance. The RTP object should use the 'cookie'
+            for calls back into the Application (to request data, for instance)
+        """
+    def createRTPSocket(self, fromIP, withSTUN):
+        """ Create the RTP socket. Use 'fromIP' as this end's IP address
+            (note that it may be a different address to the system's - it's
+            the externally visible address, as reported by STUN. If 
+            withSTUN is true, use STUN to discover the external port numbers
+            for RTP/RTCP
         """
 
-    def startDTMF(self, digit):
-        """ Start sending DTMF digit 'digit'
+    def getVisibleAddress(self):
+        """ Returns the IP address for this end of the RTP connection.
         """
 
-    def stopDTMF(self, digit):
-        """ Stop sending DTMF digit 'digit'
+    def startSendingAndReceiving(self):
+        """ Start the timer loop that delivers and receives packets.
         """
+
+    def stopSendingAndReceiving(self):
+        """ Stop the timer loop that delivers and receives packets.
+        """
+
+    
 
 class AddressBook(Interface):
     """ Address Book interface
