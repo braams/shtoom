d2e3bb20f9431746b60902597be5d5869994fa9f
anthony <>
2004-02-16 14:29:00 +0000

    new options for registration

--- shtoom/prefs.py
+++ shtoom/prefs.py
@@ -1,11 +1,16 @@
 # Copyright (C) 2004 Anthony Baxter
-# $Id: prefs.py,v 1.6 2004/01/13 14:20:52 anthonybaxter Exp $
+# $Id: prefs.py,v 1.7 2004/02/16 14:29:00 anthony Exp $
 
 localip = None
 localport = None
-username = 'anthony baxter'
+username = None
+userdetail = None
 email_address = 'anthony@interlink.com.au'
 ui = None
 audio = None
 audio_infile = None
 audio_outfile = None
+register_uri = None
+register_authuser = None
+register_authpasswd = None
+
