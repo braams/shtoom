d4843b3b3a45ccb0c3ca37058edf8e15e681c808
dp <>
2004-04-01 23:49:47 +0000

    Adding CoreAudio UI

---
+++ shtoom/audio/coreaudio.py
@@ -0,0 +1,51 @@
+# Copyright (C) 2004 Anthony Baxter
+
+
+from numarray import zeros, Float32
+
+inputBuffer = zeros((44100*2*60*1, ), Float32)
+
+### XXX This is just a copy of fileaudio.py still
+
+from converters import MultipleConv
+
+opened = None
+
+class AudioFromFiles:
+    def __init__(self, infile, outfile):
+        self.infile = infile
+        self.outfile = outfile
+        self.reopen()
+
+    def read(self):
+        return self._infp.read(320)
+
+    def write(self, bytes):
+        return self._outfp.write(bytes)
+
+    def reopen(self):
+        if self.infile is not None:
+            self._infp = open(self.infile, 'rb')
+        else:
+            self._infp = None
+        if self.outfile is not None:
+            self._outfp = open(self.outfile, 'wb')
+        else:
+            self._outfp = None
+
+    def close(self):
+        if self._infp is not None:
+            self._infp.close()
+        if self._outfp is not None:
+            self._outfp.close()
+
+def getAudioDevice(mode):
+    from __main__ import app
+    global opened
+    if opened is None:
+        opened = MultipleConv(AudioFromFiles(app.getPref('audio_infile'), app.getPref('audio_outfile')))
+    return opened
+
+def getFileAudio(infile, outfile):
+    return MultipleConv(AudioFromFiles(infile, outfile))
+
---
+++ shtoom/ui/cocoashtoom.py
@@ -0,0 +1,33 @@
+
+# Copyright (C) 2004 Anthony Baxter
+# $Id: tkshtoom.py,v 1.6 2004/03/01 13:44:03 anthony Exp $
+#
+
+
+def shutdown():
+    try:
+        import itimer
+        itimer.setitimer(itimer.ITIMER_REAL, 0.0, 0.0)
+    except:
+        pass
+    from twisted.internet import reactor
+    reactor.stop()
+
+def main(application):
+    import sys
+    from twisted.internet import reactor
+    from twisted.python import log
+    from twisted.internet import tksupport
+
+    from shtoom.ui.tkui import ShtoomMainWindow
+    UI = ShtoomMainWindow()
+    tksupport.install(UI.getMain())
+    UI.connectApplication(application)
+    print "UI done"
+    log.startLogging(UI.getLogger(), setStdout=False)
+    return UI
+
+
+
+if __name__ == "__main__":
+    main()
