50d4b955e8d8af9edfa43afbb9bc5ae71842f3d0
anthony <>
2004-05-05 12:08:47 +0000

    holy crap. it accepts calls

--- shtoom/doug/voiceapp.py
+++ shtoom/doug/voiceapp.py
@@ -14,39 +14,100 @@ from shtoom.doug.events import *
 from shtoom.doug.exceptions import *
 from shtoom.doug.statemachine import StateMachine
 from twisted.internet import reactor
+from shtoom.audio.converters import DougConverter
 
 class VoiceApp(StateMachine):
 
     def __init__(self, defer, **kwargs):
-        self._playoutList = []
-        self._recordDest = None
-        self._connected = None
-        self._silenceSource = SilenceSource()
-        self._legConnect(self._silenceSource)
+        self.__playoutList = []
+        self.__recordDest = None
+        self.__connected = None
+        self.__currentDTMFKey = None
+        self.__collectedDTMFKeys = ''
+        self.__silenceSource = SilenceSource()
+        self._legConnect(self.__silenceSource)
+        self.__converter = DougConverter()
         super(VoiceApp, self).__init__(defer, **kwargs)
+
+    def va_listFormats(self):
+        return self.__converter.listFormats()
+
+    def va_selectFormat(self, format):
+        return self.__converter.selectFormat(format)
 
     def _legConnect(self, target):
         target.app = self
-        old, self._connected = self._connected, target
-        old.app = None
+        old, self.__connected = self.__connected, target
+        if old:
+            old.app = None
         return old
 
     def _playNextItem(self):
-        if not self._playoutList:
-            self._legConnect(self._silenceSource)
+        if not self.__playoutList:
+            self._legConnect(self.__silenceSource)
             self._triggerEvent(MediaPlayContentDoneEvent)
         else:
-            next = self._playoutList.pop(0)
+            next = self.__playoutList.pop(0)
             next = convertToSource(next, 'r')
             self._legConnect(next)
 
     def _maybeStartPlaying(self):
         # If we're not already playing, switch out the silence
-        if self._connected is self._silenceSource:
+        if self.__connected is self.__silenceSource:
             self._playNextItem()
 
+    def va_startDTMFevent(self, dtmf):
+        c = self.__currentDTMFKey 
+        if c:
+            if c is not dtmf:
+                self.va_stopDTMFevent(c)
+                self.__currentDTMFKey = dtmf
+                self._inboundDTMFKeyPress(dtmf)
+            else:
+                # repeat
+                pass
+           
+
+    def _inboundDTMFKeyPress(self, dtmf):
+        if self.__dtmfSingleMode:
+            self._triggerEvent(DTMFReceivedEvent(dtmf))
+        else:
+            self.__collectedDTMFKeys += dtmf
+            if dtmf in ('#', '*'):
+                dtmf, self.__collectedDTMFKeys = self.__collectedDTMFKeys, ''
+                self._triggerEvent(DTMFReceivedEvent(dtmf))
+                
+
+    def va_stopDTMFevent(self, dtmf):
+        # For now, I only care about dtmf start events
+        pass
+
+    def va_giveRTP(self):
+        # returns (format, RTP)
+        data = self.__connected.read()
+        if data:
+            data = self.__converter.convertOutbound(data)
+            fmt = self.__converter.getFormat()
+            return fmt, data
+        return None, None # comfort noise
+
+    def va_receiveRTP(self, format, data):
+        data = self.__converter.convertInbound(format, data)
+        self.__connected.write(data)
+
+    def va_start(self):
+        self._start(callstart=0)
+
+    def va_callstart(self, inboundLeg):
+        self._triggerEvent(CallStartedEvent(inboundLeg))
+
+    def va_abort(self):
+        raise NotImplementedError
+
     def mediaPlay(self, playlist):
-        self._playoutList.extend(playlist)
+        if isinstance(playlist, basestring):
+            playlist = [playlist]
+        self.__playoutList.extend(playlist)
         self._maybeStartPlaying()
 
     def mediaRecord(self, dest):
@@ -54,21 +115,21 @@ class VoiceApp(StateMachine):
         self._legConnect(dest)
 
     def mediaStop(self):
-        old = self._legConnect(self._silenceSource)
+        old = self._legConnect(self.__silenceSource)
         if old.isPlaying():
             old.close()
-            self._playoutList = []
+            self.__playoutList = []
         if old.isRecording():
             old.close()
-            self._recordDest = None
+            self.__recordDest = None
 
     def isPlaying(self):
-        return self._connected.isPlaying()
+        return self.__connected.isPlaying()
 
     def isRecording(self):
-        return self._connected.isRecording()
+        return self.__connected.isRecording()
 
     def dtmfMode(self, single=False, timeout=0):
-        self._dtmfSingleMode = single
+        self.__dtmfSingleMode = single
         # XXX handle timeout
 
