a1c177d7b0787b56a211298780c4282f482888b5
anthony <>
2004-05-25 03:39:05 +0000

    comments, cleanup, docstrings

--- shtoom/doug/dtmfdetect.py
+++ shtoom/doug/dtmfdetect.py
@@ -1,30 +1,44 @@
 """
-    Extraordinarily badly written DTMF detection code. This is just 
-    "something that works". 
+    Extraordinarily brutal DTMF detection code. This is just 
+    "something that works". Anyone who's got a better grasp
+    of signal processing than me (which wouldn't be hard - I
+    last studied it 12 years ago, and haven't touched it since
+    then) is encouraged to suggest a better approach. I'm not
+    that happy with the getpeaks() code, to be honest the 8 times
+    greater than average level seems somehow hacky. If you are 
+    interested, let me know and I can provide you with some sample
+    files to experiment on.
 """
 
+# DTMF tones (aka "the beeps when you hit a keypad consist of a pair
+# of sine waves. The table of the sine waves is:
 
-# DTMF frequencies.
-#      1209   1336   1477   1633
-#  697   1      2      3      A     27.88
-#  770   4      5      6      B     30.8
-#  852   7      8      9      C     34.08
-#  941   *      0      #      D     37.64
-#      48.36  53.44  59.08  65.32
+#             1209   1336   1477   1633    scaled
+#         697   1      2      3      A     27.88
+#         770   4      5      6      B     30.8
+#         852   7      8      9      C     34.08
+#         941   *      0      #      D     37.64
+# scaled      48.36  53.44  59.08  65.32
 
-def getSineWave(freq):
+# 'scaled' is the number divided by 25 - since we're examining 
+# 40ms of audio at a time (1/25th of a second). 
+
+def getSineWave(freq, samplecount=320):
+    """ Generate a sine wave of frequency 'freq'. The samples are
+        generated at 8khz. The first 'samplecount' samples will be
+        returned.
+    """
     from numarray import sin, arange
     from math import pi
     sine = sin(arange(8000.0)/(8000.0/freq) * 2.0 * pi)
-    sine = sine[:320]
+    sine = sine[:samplecount]
     return sine
 
 
 class DtmfDetector:
-
+    "This class detects DTMF tones from an audio stream."
     def __init__(self):
-        from numarray import add, fabs, abs, log, greater, nonzero
-        from sets import Set 
+        # Store away pre-saved FFTs for each of the eight frequencies.
         self.freqmatch = {}
         self.dtmf = { 
             (697,1209):'1', (697,1336):'2',(697,1477):'3',(697,1633):'A',
@@ -39,14 +53,22 @@ class DtmfDetector:
                 self.freqmatch[peak] = val
 
     def detect(self, sample):
+        """ Test for DTMF in a sample. Returns either a string with the 
+            DTMF digit, or the empty string if none is present. Accepts
+            a string of length 640, representing 320 16 bit signed PCM
+            samples (host endianness).
+        """
         import numarray
         from sets import Set
-
         a = numarray.fromstring(sample, numarray.Int16)
+        if len(a) != 320:
+            raise ValueError, "only accepts samples of length 320 (40ms) for now"
         peaks = self.getpeaks(a)
         matched = Set()
         for p in peaks:
             matched.add(self.freqmatch.get(p))
+        # If we got a None, that means there was a significant component 
+        # that wasn't a DTMF frequency.
         if None in matched:
             return ''
         else:
@@ -55,26 +77,35 @@ class DtmfDetector:
             return self.dtmf.get(tuple(m), '')
 
     def getpeaks(self, a):
+        # This could be better.
         from numarray.fft import real_fft
-        from numarray import add, fabs, abs, log, greater, nonzero
+        from numarray import add, abs, greater, nonzero
         res = real_fft(a)
         res1 = abs(res)
         mres = add.reduce(res1)/len(res1)
         if not mres:
             return []
+        # Here's the bit I'm uncomfortable with. The 'eight times average'
+        # seems a hack. On the other hand, it works for me.
         res2 = res1/mres
         peaks = list(nonzero(greater(res2, 8))[0])
         return peaks
 
 def test():
     import sys
+    if len(sys.argv) != 2:
+        print "usage: dtmfdetect.py <file>"
+        print "file should be 8KHz raw 16 bit signed samples"
+        print "use sox infile.au -s -w testfile.raw to make this"
+        sys.exit(1)
     audio = open(sys.argv[1], 'r')
-    # sunau converts for us.
     D = DtmfDetector()
     old = ''
     offs = 0.0
     prev = None
     while True: 
+        # To better replicate how shtoom uses this code, we read in 20ms
+        # samples, then analyse a sliding window of size 2. 
         samp = audio.read(320)
         if len(samp) < 320:
             break
