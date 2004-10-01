7533a688172a1d6388a87fa79823a6785ec73439
anthony <>
2004-10-01 15:08:32 +0000

    more tests

--- shtoom/test/test_douglegs.py
+++ shtoom/test/test_douglegs.py
@@ -4,11 +4,18 @@ from twisted.trial import unittest
 import twisted.trial.util
 #twisted.trial.util.wait(testdef)
 
-from shtoom.doug.leg import Bridge, BridgeSource
+from shtoom.doug.leg import Bridge, BridgeSource, Leg
 
-class FakeLeg:
+class FakeLegForBridging:
     def _connectSource(self, source):
+        source.leg = self
         self.source = source
+
+    def _sourceDone(self, source):
+        self.source = None
+
+    def mediaStop(self):
+        self.source.close()
 
     def read(self):
         return self.source.read()
@@ -16,12 +23,12 @@ class FakeLeg:
     def write(self, bytes):
         return self.source.write(bytes)
 
-class LegBridgingTest(unittest.TestCase):
+class DougLegTests(unittest.TestCase):
 
     def testLegBridgingSimple(self):
         ae = self.assertEquals
-        l1 = FakeLeg()
-        l2 = FakeLeg()
+        l1 = FakeLegForBridging()
+        l2 = FakeLegForBridging()
         b = Bridge(leg1=l1, leg2=l2)
         l1.write('hello')
         ae(l2.read(), 'hello')
@@ -32,3 +39,20 @@ class LegBridgingTest(unittest.TestCase):
         l2.write('hello4')
         ae(l2.read(), 'hello3')
         ae(l1.read(), 'hello4')
+        l1.mediaStop()
+        ae(l1.source, None)
+        ae(l2.source, None)
+
+    def test_legConstructor(self):
+        from shtoom.sip import Dialog
+        ae = self.assertEquals
+        d = Dialog()
+        cookie = 'NCookie'
+        l = Leg(cookie, d)
+        ae(l.getCookie(), cookie)
+        ae(l.getDialog(), d)
+        l.setCookie('foo')
+        ae(l.getCookie(), 'foo')
+        # We should be connected to silence at this point
+        ae(l.leg_giveRTP(), None)
+
