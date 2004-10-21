727eceda406e681b39852f8fb822642d8ce3e5fe
zooko <>
2004-10-21 11:48:15 +0000

    fix shallow bug: add shtoom.address to setup.py's list of packages

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     author = "Anthony Baxter",
     author_email = "anthony@interlink.com.au",
     url = 'http://shtoom.divmod.org/',
-    packages = ['shtoom', 'shtoom.multicast', 'shtoom.avail',
+    packages = ['shtoom', 'shtoom.address', 'shtoom.multicast', 'shtoom.avail',
                 'shtoom.ui', 'shtoom.rtp', 'shtoom.ui.qtui',
                 'shtoom.ui.gnomeui', 'shtoom.ui.qtui',
                 'shtoom.ui.textui', 'shtoom.ui.tkui', 'shtoom.ui.wxui',
