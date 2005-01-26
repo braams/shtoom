32d5f33d2dd7f7f8ed512bd50fdcde1b00688054
anthony <>
2005-01-26 12:43:42 +0000

    use shtoom.avail.ui - as a bonus, if you specify a user interface, and it isn't available, you get a useful error.

--- shtoom/ui/uiselect.py
+++ shtoom/ui/uiselect.py
@@ -2,81 +2,28 @@
 
 # Copyright (C) 2004 Anthony Baxter
 
-def tryTextInterface(application):
-    import sys
-    from shtoom.ui.textshtoom import main
-    return main(application)
-
-def tryQtInterface(application):
-    import sys
-    try:
-        import qt
-    except ImportError:
-        qt = None
-    if qt is not None:
-        from shtoom.ui.qtshtoom import main
-        return main(application)
-
-def tryWxInterface(application):
-    import sys
-    try:
-        import wx
-    except ImportError:
-        wx = None
-    if wx is not None:
-        from shtoom.ui.wxshtoom import main
-        return main(application)
-
-
-def tryTkInterface(application):
-    import sys
-    try:
-        import Tkinter
-    except ImportError:
-        Tkinter = None
-    if Tkinter is not None:
-        try:
-            from shtoom.ui.tkshtoom import main
-            return main(application)
-        except:
-            pass
-
-def tryGnomeInterface(application):
-    import sys
-    try:
-        import pygtk
-        pygtk.require("2.0")
-        import gnome.ui
-    except ImportError:
-        pass
-    try:
-        import gtk
-        import gtk.glade
-    except ImportError:
-        gtk = None
-    if gtk is not None:
-        from shtoom.ui.gnomeshtoom import main
-        return main(application)
+from shtoom.avail import ui
 
 def findUserInterface(application, prefui):
-    ui = None
     if prefui:
         if prefui.lower() == 'qt':
-            ui = tryQtInterface(application)
+            main = ui.getQtInterface(fail=True)
         elif prefui.lower()[:2] == 'tk':
-            ui = tryTkInterface(application)
+            main = ui.getTkInterface(fail=True)
         elif prefui.lower() == "gnome":
-            ui = tryGnomeInterface(application)
+            main = ui.getGnomeInterface(fail=True)
         elif prefui.lower() == "wx":
-            ui = tryWxInterface(application)
+            main = ui.getWxInterface(fail=True)
         elif prefui.lower() == "text":
-            ui = tryTextInterface(application)
-    if ui is not None:
-        return ui
-    for attempt in ( tryQtInterface, tryGnomeInterface, tryWxInterface,
-                    tryTkInterface, tryTextInterface, ):
-        ui = attempt(application)
-        if ui is not None:
-            return ui
+            main = ui.getTextInterface(fail=True)
+        else:
+            raise ValueError('ui %s not known'%(prefui))
+        return main(application)
+    for attempt in ( ui.getGnomeInterface, ui.getQtInterface, 
+                     ui.getWxInterface, ui.getTkInterface, 
+                     ui.getTextInterface, ):
+        main = attempt(fail=False)
+        if main is not None:
+            return main(application)
     # Other interfaces here
     raise RuntimeError,  "Couldn't load _any_ userinterfaces"
