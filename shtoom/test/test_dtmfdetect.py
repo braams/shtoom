fa5a827ea58be2fdbaaa246648cda2665fb651b4
anthony <>
2005-02-15 13:05:51 +0000

    dtmf detection

---
+++ shtoom/test/test_dtmfdetect.py
@@ -0,0 +1,28 @@
+# Copyright (C) 2004 Anthony Baxter
+"""Tests for shtoom.doug.dtmfdetect
+"""
+
+from twisted.trial import unittest
+def getDTMFAudioFile():
+    from twisted.python.util import sibpath
+    return sibpath(__file__, 'dtmftestfile.raw')
+
+class DTMFDetectTest(unittest.TestCase):
+
+    def test_dtmfdetection(self):
+        from shtoom.doug.dtmfdetect import DtmfDetector
+        fp = open(getDTMFAudioFile(),'rb')
+        dtmf = DtmfDetector()
+        seen = []
+        cur = None
+        while True:
+            data = fp.read(640)
+            if len(data) != 640: 
+                break
+            digit = dtmf.detect(data)
+            if digit != cur:
+                seen.append(digit)
+                cur = digit
+        self.assertEquals(seen, ['', '3', '', '1', '', '4', '', '1', '', '#'])
+
+            
