ea3d4f006b730659cc4bced63f6b3c6e53e0b208
anthony <>
2005-03-28 13:14:30 +0000

    updates to new schema methods

--- shtoom/ui/tkui/prefs.py
+++ shtoom/ui/tkui/prefs.py
@@ -3,7 +3,7 @@
 from Tkinter import *
 
 from tkSimpleDialog import Dialog
-from shtoom.Options import NoDefaultOption
+from shtoom.Options import NoDefaultOption, getPrettyName
 
 
 class PreferencesDialog(Dialog):
@@ -22,10 +22,10 @@ class PreferencesDialog(Dialog):
             tab = Frame(notebook())
             for optnumber, option in enumerate(group):
                 optf = Frame(tab)
-                label = Label(optf, text=option.getPrettyName())
+                label = Label(optf, text=getPrettyName(option))
                 label.pack(fill=Y, side=LEFT)
-                if option.optionType in ( 'String', 'Number', 'Password' ):
-                    if option.optionType == 'Password':
+                if option.type in ( 'String', 'Integer', 'Password' ):
+                    if option.type == 'Password':
                         edit = Entry(optf, show='*')
                     else:
                         edit = Entry(optf)
@@ -35,13 +35,13 @@ class PreferencesDialog(Dialog):
                     edit.pack(fill=BOTH, side=RIGHT)
                     get = edit.get
                     edit = ( optf, label, edit )
-                elif option.optionType == 'Choice':
+                elif option.type == 'Choice':
                     rbf = Frame(optf)
                     buttons = []
-                    name = option.getName()
-                    val = option.getValue()
+                    name = option.name
+                    val = option.value
                     self.choiceoptions[name] = val
-                    for c in option.getChoices():
+                    for c in [x.value for x in option]:
                         if c == val:
                             E = 1
                         else:
@@ -60,8 +60,8 @@ class PreferencesDialog(Dialog):
                     get = lambda opt=name: self.choiceoptions[opt]
                     edit = ( rbf, buttons )
                 elif option.optionType == 'Boolean':
-                    name = option.getName()
-                    val = option.getValue()
+                    name = option.name
+                    val = option.value
                     self.booloptions[name] = val
                     b = Checkbutton(optf, variable=self.booloptions[name],
                                     command=lambda o=self.booloptions, n=name: o.__setitem__(n, not o[n]) )
@@ -70,10 +70,10 @@ class PreferencesDialog(Dialog):
                     get = lambda opt=name: self.booloptions[opt]
                 else:
                     raise ValueError, "unknown option type %s"%(option.optionType)
-                optf.pack(fill=X, side=TOP)
+                optf.pack(fill=X, side=TOP, ipady=4)
                 if edit is not None:
-                    self.options[option.getName()] = ( option.optionType, get, edit )
-            notebook.add_screen(tab, group.getName())
+                    self.options[option.name] = ( option.type, get, edit )
+            notebook.add_screen(tab, group.name)
         return notebook()
 
     def apply(self):
