96eb014b1edfc265a5b9cce427c3737bd90effaa
anthony <>
2005-05-08 14:52:45 +0000

    icon

---
+++ shtoom/ui/qtui/authdialog.ui.h
@@ -0,0 +1,35 @@
+/****************************************************************************
+** ui.h extension file, included from the uic-generated form implementation.
+**
+** If you want to add, delete, or rename functions or slots, use
+** Qt Designer to update this file, preserving your code.
+**
+** You should not define a constructor or destructor in this file.
+** Instead, write your code in functions called init() and destroy().
+** These will automatically be called by the form's constructor and
+** destructor.
+*****************************************************************************/
+
+
+void ShtoomAuthenticationDialog::userEntry_returnPressed()
+{
+
+}
+
+
+void ShtoomAuthDialog::passwdEntry_returnPressed()
+{
+
+}
+
+
+void ShtoomAuthDialog::cancelButton_pressed()
+{
+
+}
+
+
+void ShtoomAuthDialog::okButton_pressed()
+{
+
+}
