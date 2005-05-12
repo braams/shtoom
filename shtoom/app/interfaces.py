fda27b39221737c1ca1855cd78a49edf5dbe78c6
anthony <>
2005-05-12 14:23:17 +0000

    make ApplicationSIPInterface not lie

--- shtoom/app/interfaces.py
+++ shtoom/app/interfaces.py
@@ -8,29 +8,63 @@ class ApplicationSIPInterface(Interface):
        the SIP layer uses.
     """
 
+    def getPref(self, pref):
+        """ Return a preference setting """
+
     def acceptCall(self, call):
         """ Setup a new call.
-            Returns  deferred - .callback() will be invoked if
+            Passed a sip.Call object.
+            Returns a deferred - .callback() will be invoked if
             the call is to be accepted, or .errback() if the call is to be
             rejected.  The callback will return the callcookie - callcookie
             is a unique identifier used for all dealings with the Application
             in the future about this call.
         """
 
-    def startCall(self, callcookie, cb):
-        """ Call setup is complete, the call is now live. Accepts a callback
-            which is invoked when the application wishes to terminate the call.
-            The callback is passed a reason for the call teardown.
+    def startCall(self, callcookie, oksdp, cb):
+        """ Call setup is complete, the call is now live. 
+            Passed the final SDP for the call, and a callback which should
+            be invoked when the application wishes to terminate the call.
+            The callback will be passed a reason for the call teardown.
         """
 
     def endCall(self, callcookie, reason):
-        """ Other end has terminated the call.
+        """ Other end has terminated the call. Reason is a string.
         """
 
     def ringBack(self):
         """ The other side sent a "180 Ringing".  This is purely informational 
             -- the app may wish to, for example, play a ringback sound to the 
             user.
+        """
+
+    def notifyEvent(self, event, eventArg):
+        """ Notify the app that something interesting has happened. eventArg
+            depends on the event.
+            discoveredIP : local IP address (a string)
+            discoveredStunnedIP : externally visible address (host, port)
+        """
+
+    def authCred(self, method, uri, realm, retry=False):
+        """ Get auth credentials. Will return a deferred which when triggered
+            will be passed either (username, password) or None (if no auth
+            is available). retry means that the auth failed (so, for instance,
+            the app should re-ask for auth credentials).
+        """
+
+    def getSDP(self, callcookie, otherSDP=None):
+        """ Returns a shtoom.sdp.SDP instance for this call.
+            if otherSDP is not None, do an intersection with it and return
+            the result.
+        """
+
+    def debugMessage(self, message):
+        """ Display a debugging message
+        """
+
+    def statusMessage(self, message):
+        """ Display a status message. This should replace the previous 
+            status message
         """
 
 class ApplicationRTPInterface(Interface):
