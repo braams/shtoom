ce7f6477443f62ec032fe999e181cdff283d735c
anthony <>
2005-10-03 12:20:21 +0000

    play and record are separate since r1593

--- scripts/shtoomcu.py
+++ scripts/shtoomcu.py
@@ -71,6 +71,7 @@ class ConferencingApp(VoiceApp):
         self.mediaStop()
         self.conf = newConferenceMember(self.roomname, self.leg)
         self.mediaPlay([self.conf])
+        self.mediaRecord([self.conf])
         return ( (MediaDoneEvent, self.startConference),
                  (CallEndedEvent,  self.allDone),
                  (DTMFReceivedEvent,      IGNORE_EVENT),
